91ec72fa1144beb5c4844f8b3b92260a88485191
Tuomas <Tuomas@TUOMASPC>
2017-04-25 10:46:43 +0300

    add icon, add guide.

--- Laivanupotus/Battleship/Properties/Resources.Designer.cs
+++ Laivanupotus/Battleship/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace Battleship.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Battleshipicon {
+            get {
+                object obj = ResourceManager.GetObject("Battleshipicon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream explosion {
